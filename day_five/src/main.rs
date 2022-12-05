f354bfb77fb58ad12c06a5dc9cf7448ffa46098d
Carson McKinstry <carsonm@spotify.com>
2022-12-05 13:37:44 +0100

    remove handwritten stack stuff

--- day_five/src/main.rs
+++ day_five/src/main.rs
@@ -4,18 +4,6 @@ use std::fs;
 
 use stack::Stack;
 
-// Initial State
-// it'll be faster to just bring this in manually
-//             [J] [Z] [G]
-//             [Z] [T] [S] [P] [R]
-// [R]         [Q] [V] [B] [G] [J]
-// [W] [W]     [N] [L] [V] [W] [C]
-// [F] [Q]     [T] [G] [C] [T] [T] [W]
-// [H] [D] [W] [W] [H] [T] [R] [M] [B]
-// [T] [G] [T] [R] [B] [P] [B] [G] [G]
-// [S] [S] [B] [D] [F] [L] [Z] [N] [L]
-//  1   2   3   4   5   6   7   8   9
-
 struct Instruction {
     pub(crate) m: usize,
     pub(crate) src: usize,
